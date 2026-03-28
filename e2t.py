ec354fa7759f147b4d9e552e1668de68c9cdb481
chronosirius <chronosirius@gmail.com>
2026-03-28 11:44:07 -0700

    e2t

--- e2t.py
+++ e2t.py
@@ -1,1 +1,15 @@
-import vec2text
+import vec2text as v2t
+import torch
+from sentence_transformers import SentenceTransformer as ST
+
+encoder = ST("BAAI/bge-large-en-v1.5")
+decoder = v2t.load_pretrained_corrector("jxm/gtr__nq__32")
+target_embeddings = torch.randn(1, 1024).cpu()
+
+reconstructed_text = v2t.invert_embeddings(
+    embeddings=target_embeddings,
+    corrector=decoder,
+    num_steps=20
+)
+
+print(reconstructed_text)
